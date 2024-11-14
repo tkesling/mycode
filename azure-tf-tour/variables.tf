e44fa9ee10bf5665373d3530dbd27581f5b5e91f
tkesling <kesling@gmail.com>
2024-11-14 17:02:42 +0000

    terraform and Azure

---
+++ azure-tf-tour/variables.tf
@@ -0,0 +1,12 @@
+# this is now an "input" value 
+variable "build_location" {
+  type = string
+  default = "westus2"
+}
+
+# this is now an "input" value 
+variable "rg_name" {
+  type = string
+  default = "myAzureRG"
+}
+
